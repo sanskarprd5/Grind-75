1741cf0437a484e36f6e9977c564484736e59801
Sanskar Parida <138426751+sanskarprd5@users.noreply.github.com>
2023-08-25 11:18:58 +0530

    Create InvertBinaryTree.js
    Given the root of a binary tree, invert the tree, and return its root.
    
    Example-1:
     Input: root = [4,2,7,1,3,6,9]
    Output: [4,7,2,9,6,3,1]

---
+++ InvertBinaryTree.js
@@ -0,0 +1,15 @@
+var invertTree = function (root) {
+  if (root === null) {
+    return null;
+  }
+  let temp = root.left;
+  root.left = root.right;
+  root.right = temp;
+  invertTree(root.left);
+  invertTree(root.right);
+
+  return root;
+};
+
+const res = invertTree([4, 2, 7, 1, 3, 6, 9]);
+console.log(invertTree);
